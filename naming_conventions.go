3ecbd46f4e5576f8543d7c2a9ae4efc87db3ae88
Noyan <8445958+mindcool@users.noreply.github.com>
2025-11-16 13:03:59 -0700

    Work on naming conventions

--- naming_conventions.go
+++ naming_conventions.go
@@ -1,5 +1,35 @@
 package main
 
+import "fmt"
+
+type Employee struct {
+	FirstName string
+	LastName  string
+	Age       int
+}
+
 func main() {
-	
+	// Pascal case
+	// PascalCase looks like this
+	// Ex. CalculateArea, UserInfo, NewHttpRequest
+	// Structs, interfaces, enums ...
+
+	// Snake case
+	// snake_case looks like this
+	// Ex. user_id, first_name, http_request
+	// Snake case commonly used for variables, file names
+
+	// UPPERCASE
+	// Upper case used explicity named for constants
+	// PI, AREAMULTIPLIER
+	// Constants are immutable
+
+	// Mix Case
+	// Ex javaScript, htmlDocument, isValid
+	// Dealing with external libraries or following other language(s) consistancy
+
+	// Package names always should be without underscores and should always be lowercase
+	const MAXENTRIES = 5
+	var employeeID = 1001
+	fmt.Println("EmployeeID: ", employeeID)
 }
